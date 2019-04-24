b04e9f2f1a4a5432028239be38698ef82a9fdcb0
doughertyg <doughertyg37@gmail.com>
2019-04-24 16:56:18 -0700

    Some actions written

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,14 @@
-var changeVideoList = (videos) => ({
+var changeVideoList = (videos) => {
   //TODO:  Return some action object to change the list of videos in VideoList.
-});
+  return {
+    type: 'CHANGE_VIDEO_LIST',
+    videos: videos
+  }
+};
+
+
+//function that updates currentVideoList 
+
+//update current videoplayer when title clicked
 
 export default changeVideoList;
